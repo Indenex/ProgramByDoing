65faad442d24be6e9d981eafd435abfeabb661c0
Jaynex <justin@indenex.com>
2020-07-12 01:40:01 -0500

    Initial commit - created a letter in java terminal

---
+++ FirstProg.java
@@ -0,0 +1,18 @@
+public class FirstProg {
+    public static void main( String[] args ) {
+
+        System.out.println("+-------------------------------------------------------------+");
+        System.out.println("|   Justin                                                    |");
+        System.out.println("|   101 Redmond Road                                          |");
+        System.out.println("|   Redmond, WA 98104                                         |");
+        System.out.println("|                                                             |");
+        System.out.println("|                                                             |");
+        System.out.println("|                                                             |");
+        System.out.println("|                                Bill Gates                   |");
+        System.out.println("|                                1 Microsoft Way              |");
+        System.out.println("|                                Redmond, WA 98104            |");
+        System.out.println("|                                                             |");
+        System.out.println("+-------------------------------------------------------------+");
+
+    }
+}
